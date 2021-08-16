195bc945ff2049e6d7aa8ebed852a78862b6e3a8
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-16 10:16:09 -0400

    Added app.get for the routes /notes, *, /api/notes and an app.post for the route /api/notes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const PORT = process.env.port || 3001;
 
@@ -8,4 +10,44 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'))
+app.use(express.static("public"));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+const readFromFile = util.promisify(fs.readFile);
+
+app.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)));
+});
+
+
+
+
+
+app.post("/api/notes", (req, res) => {
+    const {title, text} = req.body;
+
+    if(title && text){
+        const newNote = {
+            title,
+            text,
+        }
+        
+        readAndAppend(newNote, './db/db.json')
+    } else {
+        res.json('Error in posting a new note')
+    }
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});

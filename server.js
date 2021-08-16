777f1ebf2880138e381de868739e6e8bd4be2b69
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-16 09:24:36 -0400

    Rearranged the starter code repoditory structure to follow the more popular file structure

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const express = require('express');
-const fs 
+const express = require("express");
+const fs = require("fs");
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'))

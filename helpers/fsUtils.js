270a7373025b8c508843345e03661ed29360979a
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-17 13:46:52 -0400

    Added comments that explains what each function does in the fsUtils.js

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,14 +1,20 @@
+// Exports the required fs and util node modules that's needed to access the filesystem
 const fs = require("fs");
 const util = require("util");
 
+// Makes the read file from file system asynchronous 
 const readFromFile = util.promisify(fs.readFile);
 
+
+// Writes data to file system by giving it a destination and data
 const writeToFile = (destination, content) => {
   fs.writeFile(destination, JSON.stringify(content, null, 4), err =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 };
 
+// Reads from the given json file and writes data to file system 
+// Checks for errors and after it parses the json file and adds the new note obj 
 const readAndAppend = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
@@ -21,4 +27,6 @@ const readAndAppend = (content, file) => {
   });
 };
 
+
+// Exports the readFromFile, writeToFile and readAndAppend to be available to the App
 module.exports = {readFromFile, writeToFile, readAndAppend}
